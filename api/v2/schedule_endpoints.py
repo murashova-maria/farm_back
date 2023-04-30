9c4820c5461fd0b4eef7a3c649919c1dd5ab5969
RedNhight <penguin_nube@kali.localhost>
2023-04-30 23:17:45 +0300

    first

--- api/v2/schedule_endpoints.py
+++ api/v2/schedule_endpoints.py
@@ -2,34 +2,34 @@
 from api import *
 
 
-@app.post('/bots/{user_id}/schedules/')
-async def bots_schedule(user_id: str, file_name: Annotated[bytes, File()], day: Annotated[str, Form()],
-                        action: Annotated[str, Form()], time_range: Annotated[str, Form()],
-                        text: Annotated[str, Form()]):
-    try:
-        exact_time = get_randomized_date(int(day), RANGES[int(time_range)])
-        data = [user_id, action, day, time_range, exact_time]
-        social_media = UserDB.filter_users(user_id=user_id)[0]['social_media']
-        if action == 'make_post':
-            filename = f'{user_id}_post_image_{randint(0, 10000)}.jpg'
-            # Save the binary data to a file using asyncio and aiofiles
-            with open(IMG_DIR + f'{social_media}/' + filename, 'rb') as output:
-                output.write(file_name)
-            post_data = {
-                'user_id': user_id,
-                'text': text,
-                'filename': filename,
-                'status': 'None',
-                'day': day,
-                'time_range': time_range,
-                'exact_time': exact_time,
-            }
-            main_queue.put(QueuedTask(SelfPostsDB, 'create_post', post_data))
-        main_queue.put(QueuedTask(ScheduleDB, 'create_schedule', data))
-        return {'Status': "OK"}
-    except Exception as ex:
-        print(ex)
-        raise HTTPException(status_code=400, detail=[])
+# @app.post('/bots/{user_id}/schedules/')
+# async def bots_schedule(user_id: str, file_name: Annotated[bytes, File()], day: Annotated[str, Form()],
+#                         action: Annotated[str, Form()], time_range: Annotated[str, Form()],
+#                         text: Annotated[str, Form()]):
+#     try:
+#         exact_time = get_randomized_date(int(day), RANGES[int(time_range)])
+#         data = [user_id, action, day, time_range, exact_time]
+#         social_media = UserDB.filter_users(user_id=user_id)[0]['social_media']
+#         if action == 'make_post':
+#             filename = f'{user_id}_post_image_{randint(0, 10000)}.jpg'
+#             # Save the binary data to a file using asyncio and aiofiles
+#             with open(IMG_DIR + f'{social_media}/' + filename, 'rb') as output:
+#                 output.write(file_name)
+#             post_data = {
+#                 'user_id': user_id,
+#                 'text': text,
+#                 'filename': filename,
+#                 'status': 'None',
+#                 'day': day,
+#                 'time_range': time_range,
+#                 'exact_time': exact_time,
+#             }
+#             main_queue.put(QueuedTask(SelfPostsDB, 'create_post', post_data))
+#         main_queue.put(QueuedTask(ScheduleDB, 'create_schedule', data))
+#         return {'Status': "OK"}
+#     except Exception as ex:
+#         print(ex)
+#         raise HTTPException(status_code=400, detail=[])
 
 
 # @app.post('/bots/{user_id}/schedules/')
@@ -66,31 +66,31 @@ async def bots_schedule(user_id: str, file_name: Annotated[bytes, File()], day: 
 #         raise HTTPException(status_code=400, detail=[])
 
 
-# @app.post('/bots/{user_id}/schedules/')
-# async def bots_schedule(user_id: str, file_name: Annotated[bytes, File()], day: Annotated[str, Form()],
-#                         action: Annotated[str, Form()], time_range: Annotated[str, Form()],
-#                         text: Annotated[str, Form()]):
-#     try:
-#         exact_time = get_randomized_date(int(day), RANGES[int(time_range)])
-#         # data = [user_id, action, day, time_range, exact_time]
-#         # social_media = UserDB.filter_users(user_id=user_id)[0]['social_media']
-#         if action == 'make_post' and text:
-#             filename = f'{user_id}.jpg'
-#             if filename is not None:
-#                 with open("some.png", 'wb') as f:
-#                     f.write(file_name)
-#             post_data = {
-#                 'user_id': user_id,
-#                 'text': text,
-#                 'filename': filename,
-#                 'status': 'None',
-#                 'day': day,
-#                 'time_range': time_range,
-#             }
-#             print(post_data)
-#         return {'Status': "OK"}
-#     except Exception as ex:
-#         print(ex)
+@app.post('/bots/{user_id}/schedules/')
+async def bots_schedule(user_id: str, file_name: Annotated[bytes, File()], day: Annotated[str, Form()],
+                        action: Annotated[str, Form()], time_range: Annotated[str, Form()],
+                        text: Annotated[str, Form()]):
+    try:
+        exact_time = get_randomized_date(int(day), RANGES[int(time_range)])
+        # data = [user_id, action, day, time_range, exact_time]
+        # social_media = UserDB.filter_users(user_id=user_id)[0]['social_media']
+        if action == 'make_post' and text:
+            filename = f'{user_id}.jpg'
+            if filename is not None:
+                with open("some.png", 'wb') as f:
+                    f.write(file_name)
+            post_data = {
+                'user_id': user_id,
+                'text': text,
+                'filename': filename,
+                'status': 'None',
+                'day': day,
+                'time_range': time_range,
+            }
+            print(post_data)
+        return {'Status': "OK"}
+    except Exception as ex:
+        print(ex)
 
 
 @app.post('/bots/{user_id}/self_posts/')
